bbe10447af99cfc82eb9f5c288828481d0b41916
Arpan Maity <33843098+binary-greens@users.noreply.github.com>
2021-07-10 21:05:28 +0530

    rds successfully connected and adding and fetching data is done. TODO: update and delete

--- app.py
+++ app.py
@@ -1,64 +1,73 @@
 import os
+import pymysql
 
 from flask import Flask, render_template, request, redirect
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-db = SQLAlchemy(app)
 
+ENDPOINT = os.environ.get('RDS_ENDPOINT')
+USR = os.environ.get('RDS_USER')
+PASSWORD = os.environ.get('RDS_PASSWORD')
+REGION = "us-east-1"
+DBNAME = os.environ.get('RDS_DB_NAME')
 
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow())
-
-    def __repr__(self):
-        return '<Task %r>' % self.id
+try:
+    conn = pymysql.connect(user=USR, password=PASSWORD, host=ENDPOINT, autocommit=True)
+    cur = conn.cursor()
+    cur.execute("""USE todoApp""")
+    print("Database connected")
+except Exception as e:
+    print("Database connection failed due to {}".format(e))
 
 
 @app.route('/', methods=['GET', 'POST'])
-def hello_world():
+def index():
     if request.method == 'POST':
         task_content = request.form['content']
-        new_task = Todo(content=task_content)
         try:
-            db.session.add(new_task)
-            db.session.commit()
+            sql = '''INSERT into todo (content) VALUES ('%s')''' % task_content
+            cur.execute(sql)
             return redirect('/')
         except:
             return 'There was an issue adding your task'
     else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
+        try:
+            sql = '''SELECT * from todo'''
+            cur.execute(sql)
+            tasks = cur.fetchall()
+        except:
+            return 'There was an issue rendering all data'
+        for task in tasks:
+            print(task)
         return render_template('index.html', tasks=tasks)
 
 
-@app.route('/delete/<int:id>')
-def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
-    try:
-        db.session.delete(task_to_delete)
-        db.session.commit()
-        return redirect('/')
-
-    except:
-        return 'There was a problem deleting the task'
-
-
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
-def update(id):
-    selected_task = Todo.query.get_or_404(id)
-    if request.method == 'POST':
-        selected_task.content = request.form['content']
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue updating your task'
-    else:
-        return render_template('update.html', selected_task=selected_task)
+#
+#
+# @app.route('/delete/<int:id>')
+# def delete(id):
+#     task_to_delete = Todo.query.get_or_404(id)
+#     try:
+#         db.session.delete(task_to_delete)
+#         db.session.commit()
+#         return redirect('/')
+#
+#     except:
+#         return 'There was a problem deleting the task'
+#
+#
+# @app.route('/update/<int:id>', methods=['GET', 'POST'])
+# def update(id):
+#     selected_task = Todo.query.get_or_404(id)
+#     if request.method == 'POST':
+#         selected_task.content = request.form['content']
+#         try:
+#             db.session.commit()
+#             return redirect('/')
+#         except:
+#             return 'There was an issue updating your task'
+#     else:
+#         return render_template('update.html', selected_task=selected_task)
 
 
 if __name__ == '__main__':
